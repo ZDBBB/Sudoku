8c7defa7b7a4955703f6880cd8c5e7e4617210cd
José Vitor Abilhôa Vincoski <jose.vitor@csistemas.com.br>
2022-04-20 10:39:10 -0300

    Versão inicial grid sudoku

---
+++ sudoku/src/App.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Bloco from './Components/Bloco';
+
+function App() {
+  return (
+
+
+    <div className="App">
+      <table>
+        <tr>
+          <th>
+            <Bloco/>
+          </th>
+          <th>
+            <Bloco/>
+          </th>
+          <th>
+            <Bloco/>
+          </th>
+        </tr>
+        <tr>
+          <th>
+            <Bloco/>
+          </th>
+          <th>
+            <Bloco/>
+          </th>
+          <th>
+            <Bloco/>
+          </th>
+        </tr>
+        <tr>
+          <th>
+            <Bloco/>
+          </th>
+          <th>
+            <Bloco/>
+          </th>
+          <th>
+            <Bloco/>
+          </th>
+        </tr>
+      </table>
+    </div>
+  );
+}
+
+export default App;
---
+++ sudoku/src/Components/Bloco/index.tsx
@@ -0,0 +1,12 @@
+import { click } from "@testing-library/user-event/dist/click"
+
+function Bloco(){
+    return(
+        <div style={{width: 150, height: 150, backgroundColor: '#a19f99', borderRadius: 10}}>
+        1
+        </div>
+    )
+}
+
+
+export default Bloco
