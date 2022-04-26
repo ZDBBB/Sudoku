c2819f429ded62dcd7b4f4ff58fc4955313b064a
jose <jose.vitor.abilhoa.vincoski@gmail.com>
2022-04-26 14:46:47 -0300

    refatorado css

--- src/Components/SudokuGrid/index.tsx
+++ src/Components/SudokuGrid/index.tsx
@@ -1,286 +1,108 @@
+import './index.css';
+
 function SudokuGrid(){
     return(
     <div className='game-container'>
-      <div className="outer-grid">
-        <div className="outer-square">
-          <div className="inner-grid">
-            <div className="inner-square">1</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">2</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">3</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">4</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">5</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">6</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">7</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">8</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">9</div>
-          </div>
+        <div className="external-grid">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+          <div className="square">8</div>
+          <div className="square">9</div>
+        </div>
+        <div className="external-grid">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+          <div className="square">8</div>
+          <div className="square">9</div>
+        </div>
+        <div className="external-grid">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+          <div className="square">8</div>
+          <div className="square">9</div>
+        </div>
+        <div className="external-grid">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+          <div className="square">8</div>
+          <div className="square">9</div>
+        </div>
+        <div className="external-grid">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+          <div className="square">8</div>
+          <div className="square">9</div>
+        </div>
+        <div className="external-grid">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+          <div className="square">8</div>
+          <div className="square">9</div>
+        </div>
+        <div className="external-grid">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+          <div className="square">8</div>
+          <div className="square">9</div>
+        </div>
+        <div className="external-grid">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+          <div className="square">8</div>
+          <div className="square">9</div>
+        </div>
+        <div className="external-grid">
+          <div className="square">1</div>
+          <div className="square">2</div>
+          <div className="square">3</div>
+          <div className="square">4</div>
+          <div className="square">5</div>
+          <div className="square">6</div>
+          <div className="square">7</div>
+          <div className="square">8</div>
+          <div className="square">9</div>
         </div>
       </div>
-      <div className="outer-grid">
-        <div className="outer-square">
-          <div className="inner-grid">
-            <div className="inner-square">1</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">2</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">3</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">4</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">5</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">6</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">7</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">8</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">9</div>
-          </div>
-        </div>
-      </div>
-      <div className="outer-grid">
-        <div className="outer-square">
-          <div className="inner-grid">
-            <div className="inner-square">1</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">2</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">3</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">4</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">5</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">6</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">7</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">8</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">9</div>
-          </div>
-        </div>
-      </div>
-      <div className="outer-grid">
-        <div className="outer-square">
-          <div className="inner-grid">
-            <div className="inner-square">1</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">2</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">3</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">4</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">5</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">6</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">7</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">8</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">9</div>
-          </div>
-        </div>
-      </div>
-      <div className="outer-grid">
-        <div className="outer-square">
-          <div className="inner-grid">
-            <div className="inner-square">1</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">2</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">3</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">4</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">5</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">6</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">7</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">8</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">9</div>
-          </div>
-        </div>
-      </div>
-      <div className="outer-grid">
-        <div className="outer-square">
-          <div className="inner-grid">
-            <div className="inner-square">1</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">2</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">3</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">4</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">5</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">6</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">7</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">8</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">9</div>
-          </div>
-        </div>
-      </div>
-      <div className="outer-grid">
-        <div className="outer-square">
-          <div className="inner-grid">
-            <div className="inner-square">1</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">2</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">3</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">4</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">5</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">6</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">7</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">8</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">9</div>
-          </div>
-        </div>
-      </div>
-      <div className="outer-grid">
-        <div className="outer-square">
-          <div className="inner-grid">
-            <div className="inner-square">1</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">2</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">3</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">4</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">5</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">6</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">7</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">8</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">9</div>
-          </div>
-        </div>
-      </div>
-      <div className="outer-grid">
-        <div className="outer-square">
-          <div className="inner-grid">
-            <div className="inner-square">1</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">2</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">3</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">4</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">5</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">6</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">7</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">8</div>
-          </div>
-          <div className="inner-grid">
-            <div className="inner-square">9</div>
-          </div>
-        </div>
-      </div>
-    </div>
     )
 }
 
